ea61eef81479b321b5758e017372651c11624733
trantrungtien9x <trantrungtien9x@gmail.com>
2025-11-30 03:48:54 +0000

    chore: update import test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nettruyen/main.dart';
+import 'package:minh_nguyet_truyen/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
